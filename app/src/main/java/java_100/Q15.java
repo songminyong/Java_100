62a25262656549cfb51c76a1b145caef662cd694
송민용 <87658538+songminyong@users.noreply.github.com>
2023-11-07 15:02:10 +0900

    test2

--- app/src/main/java/java_100/Q15.java
+++ app/src/main/java/java_100/Q15.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 
 public class Q15 
 {
-    Scanner scan = new Scanner(System.in);
-    int year = scan.nextInt();
-    int month = scan.nextInt();
-    int day = scan.nextInt();
-    System.out.printf("%04d.%02d.%02d", year, month, day);
-    scan.close();
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        int month = scan.nextInt();
+        int day = scan.nextInt();
+        System.out.printf("%04d.%02d.%02d", year, month, day);
+        scan.close();   
+    }
 }

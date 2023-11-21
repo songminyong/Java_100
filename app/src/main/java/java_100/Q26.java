f2274605c12e8c1874a668b1b42c4628f99033ca
송민용 <87658538+songminyong@users.noreply.github.com>
2023-11-21 15:37:10 +0900

    Q_26

---
+++ app/src/main/java/java_100/Q26.java
@@ -0,0 +1,13 @@
+package java_100;
+
+import java.util.Scanner;
+
+public class Q26 
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean b = scan.nextBoolean();
+        System.out.println(!b);
+        scan.close();
+    }
+}
